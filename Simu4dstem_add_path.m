bfc60cc75c283c5e249413e8183b99d88b0fc07b
Zhiyuan Ding <ZhiyuanDing@outlook.com>
2025-07-15 10:59:41 +0100

    Delete Simu4dstem_add_path.m

--- Simu4dstem_add_path.m
+++
@@ -1,3 +0,0 @@
-addpath('D:\Zhiyuan\saved_tools\multem_stable\mex_bin');
-addpath('D:\Zhiyuan\saved_tools\multem_stable\matlab_functions');
-addpath('D:\Zhiyuan\saved_tools\multem_stable\crystalline_materials');
